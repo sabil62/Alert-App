a705197c5b342f56cf8d13efaef2740cc5571972
sabil62 <shresthasabil62@gmail.com>
2020-05-10 14:26:42 +0545

    TAb bar color

--- lib/files2/tabMiddle.dart
+++ lib/files2/tabMiddle.dart
@@ -6,18 +6,35 @@ class TabBarMiddleApp extends StatefulWidget {
 }
 
 class _TabBarMiddleAppState extends State<TabBarMiddleApp> {
+  Color appBarColor = Color(0xFFD81B60);
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
           appBar: new AppBar(
+        backgroundColor: appBarColor,
         title: Text('Tab Bar'),
         bottom: TabBar(
             isScrollable: true,
             indicatorWeight: 4,
             indicatorColor: Colors.white54,
+            unselectedLabelColor: Colors.grey[200],
             indicatorPadding: const EdgeInsets.all(2),
+            onTap: (index) {
+              switch (index) {
+                case 0:
+                  appBarColor = Color(0xFFFB8C00);
+                  break;
+                case 1:
+                  appBarColor = Color(0xff73A1F9);
+                  break;
+                case 2:
+                  appBarColor = Color(0xff0f9b0f);
+                  break;
+                default:
+              }
+            },
             tabs: [
               Center(
                 child: Icon(
